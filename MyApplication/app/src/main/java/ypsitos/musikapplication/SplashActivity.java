dd10866eb6047dad89e1ca835a0fcaedc83f9533
David Sarker <dsarker27@gmail.com>
2016-03-12 14:55:47 -0500

    Splash Screen

---
+++ MyApplication/app/src/main/java/ypsitos/musikapplication/SplashActivity.java
@@ -0,0 +1,21 @@
+package ypsitos.musikapplication;
+
+/**
+ * Created by David on 3/12/16.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
