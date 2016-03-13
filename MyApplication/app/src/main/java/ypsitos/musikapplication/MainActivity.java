f621260f698f82d79f89f7b974e905b902b64339
Yanni-Psitos <ypsitos@gmail.com>
2016-03-13 10:37:06 -0400

    Contacts and DetailAct

--- MyApplication/app/src/main/java/ypsitos/musikapplication/MainActivity.java
+++ MyApplication/app/src/main/java/ypsitos/musikapplication/MainActivity.java
@@ -1,13 +1,25 @@
 package ypsitos.musikapplication;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent toDetailActivity = new Intent(MainActivity.this,DetailActivity.class);
+                startActivity(toDetailActivity);
+
             }
         });
     }
@@ -49,4 +63,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+
+    }
+
